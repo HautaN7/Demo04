4c4e89ab99fcf6eea48ac4b3298c6253e6cdb165
Taavi Hautoniemi <taavihautoniemi@hotmail.com>
2016-01-25 20:58:05 +0200

    Radio created.

---
+++ RadioApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RadioApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ RadioApp/Radio.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RadioApp
+{
+    class Radio
+    {
+        // max/min frequency & volume
+        private readonly double maxFrequency = 26000.0;
+        private readonly double minFrequency = 2000.0;
+        private double frequency;
+
+        private readonly int maxVolume = 9;
+        private readonly int minVolume = 0;
+        private int volume;
+
+        public bool On = true;
+        public double Frequency
+        {
+            get
+            {
+                return frequency;
+            }
+            set
+            {
+                if (value <= maxFrequency) frequency =  value;
+                else
+                {
+                    Console.WriteLine("Too high frequency - set to max!");
+                    frequency = maxFrequency;
+                }
+                if (value >= minFrequency) frequency = value;
+                else
+                {
+                    Console.WriteLine("Frequency too low - set to min!");
+                    frequency = minFrequency;
+                }
+            }
+        }
+        public int Volume
+        {
+            get
+            {
+                return volume;
+            }
+            set
+            {
+                if (value <= maxVolume) volume = value;
+                else
+                {
+                    Console.WriteLine("Too high volume - set to max!");
+                    volume = maxVolume;
+                }
+                if (value >= minVolume) volume = value;
+                else
+                {
+                    Console.WriteLine("Volume too low - set to min!");
+                    volume = minVolume;
+                }
+        }
+    }
+}
